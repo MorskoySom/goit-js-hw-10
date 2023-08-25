182f554217b3eb3c0c198903b36fa025bc706130
MorskoySom <Somovvitvit@gmail.com>
2023-08-25 11:16:21 +0300

    10023

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,13 +1,30 @@
 import axios from "axios";
+const elem = {
+    breedSelect: document.querySelector(".breed-select"),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+    catInfo: document.querySelector('.cat-info')
+}
+
+// function fetchBreeds() {
+//     // elem.loader.classList.remove(`loader`);
+//     return axios.get("https://api.thecatapi.com/v1/breeds");
+// }
 
 function fetchBreeds() {
-    return axios.get("https://api.thecatapi.com/v1/breeds");
+    // elem.loader.classList.remove('loader');
+    return axios.get("https://api.thecatapi.com/v1/breeds")
+    // .finally(() => {
+    //     elem.loader.classList.add('loader');
+    // });
 }
+
 
 function fetchCatByBreed(breedId) {
     let params = new URLSearchParams({
         breed_ids: breedId,
     });
+    elem.loader.classList.remove('loader');
     return axios.get(`https://api.thecatapi.com/v1/images/search?${params}`).then((response) => {
         if (!response.data) {
             new Error(response.status);
@@ -16,6 +33,9 @@ function fetchCatByBreed(breedId) {
             return response.data[0];
         }
     })
+        .finally(() => {
+            elem.loader.classList.add('loader');
+        });
 }
 
 
