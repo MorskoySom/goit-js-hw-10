8cd55ea3fb9b41c2ff7ff07656ee2429d96e597c
MorskoySom <Somovvitvit@gmail.com>
2023-08-25 11:43:42 +0300

    909

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,25 +6,33 @@ const elem = {
     catInfo: document.querySelector('.cat-info')
 }
 
-// function fetchBreeds() {
-//     // elem.loader.classList.remove(`loader`);
-//     return axios.get("https://api.thecatapi.com/v1/breeds");
-// }
-
 function fetchBreeds() {
-    // elem.loader.classList.remove('loader');
     return axios.get("https://api.thecatapi.com/v1/breeds")
-    // .finally(() => {
-    //     elem.loader.classList.add('loader');
-    // });
 }
 
+// function fetchCatByBreed(breedId) {
+//     let params = new URLSearchParams({
+//         breed_ids: breedId,
+//     });
+//     elem.loader.hidden = false;
+//     return axios.get(`https://api.thecatapi.com/v1/images/search?${params}`).then((response) => {
+//         if (!response.data) {
+//             new Error(response.status);
+//         }
+//         else {
+//             return response.data[0];
+//         }
+//     })
+//         .finally(() => {
+//             elem.loader.hidden = true;
+//         });
+// }
 
 function fetchCatByBreed(breedId) {
     let params = new URLSearchParams({
         breed_ids: breedId,
     });
-    elem.loader.classList.remove('loader');
+    elem.loader.hidden = false;
     return axios.get(`https://api.thecatapi.com/v1/images/search?${params}`).then((response) => {
         if (!response.data) {
             new Error(response.status);
@@ -34,10 +42,9 @@ function fetchCatByBreed(breedId) {
         }
     })
         .finally(() => {
-            elem.loader.classList.add('loader');
+            elem.loader.hidden = true;
         });
 }
-
 
 export { fetchBreeds, fetchCatByBreed };
 
